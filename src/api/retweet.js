4d76cc61bfe0aca6fc976a048e292198e1e96a62
Ryan Valizan <ryan@vnetkc.com>
2020-10-18 18:36:38 -0500

    :loud_sound: adding in log comments
    This will be useful for fine tuning your bots retweet habits.

--- src/api/retweet.js
+++ src/api/retweet.js
@@ -3,13 +3,25 @@ const unique = require('unique-random-array')
 const config = require('../config')
 const isReply = require('../helpers/isReply')
 const isBanned = require('../helpers/isBanned')
+const log4jConfig = require('../log4jConfig')
 
-const param = config.twitterConfig
+// Start Logging
+const consoleLol = require('console.lol')
+const log4js = require('log4js')
+log4js.configure(log4jConfig.options)
+const logger = log4js.getLogger('default.tweets')
+
+// Set log levels to match user env file setting
+logger.level = config.envVars.logLevel.toUpperCase()
+logger.trace('📝 Starting to log: retweet.js')
+
+const param = config.envVars
 const queryString = unique(param.queryString.split(','))
 
 const bot = new Twit(config.twitterKeys)
 
 const retweet = () => {
+    logger.trace('🟢 Retweet Starting')
     const query = queryString()
 
     bot.get(
@@ -22,35 +34,54 @@ const retweet = () => {
         },
         (err, data, response) => {
             if (err) {
-                console.lol('ERRORDERP: Cannot Search Tweet!, Description here: ', err)
-            } else {
-                let retweetId
-                    // grab random tweet ID to retweet - desired range for random number is [0..data.statuses.length-1]
-                let rando = Math.floor(Math.random() * data.statuses.length)
-                while (isBanned(data.statuses[rando])) {
-                    rando = Math.floor(Math.random() * data.statuses.length)
-                }
+                logger.error('Search for tweets failed with message: ', err)
+                return
+                //! console.lol('ERRORDERP: Cannot Search Tweet!, Description here: ', err)
+            }
+            let retweetId
 
-                if (!isReply(data.statuses[rando])) {
-                    retweetId = data.statuses[rando].id_str
-                    bot.post(
-                        'statuses/retweet/:id', {
-                            id: retweetId
-                        },
-                        (err, response) => {
-                            if (err) {
-                                console.log(`failed with err retweetId ${retweetId} `)
-                                console.lol('ERRORDERP: Retweet!')
-                            }
-                            console.lol(
-                                'SUCCESS: RT: ',
+            // grab random tweet ID number to retweet - desired range for random number is [0..data.statuses.length-1]
+            logger.trace('🟢 Starting search for random post to check')
+            let rando = Math.floor(Math.random() * data.statuses.length)
+            logger.debug(`Rando was assigned to: ${rando}`)
+            while (isBanned(data.statuses[rando])) {
+                logger.trace('Getting new rando number')
+                rando = Math.floor(Math.random() * data.statuses.length)
+                logger.debug(`Rando is now set to ${rando}`)
+            }
+            logger.trace('While loop checking tweet for illegal terms has completed')
+            logger.debug(`While loop completed successfully with rando: ${rando}`)
+
+            logger.trace('🕵️‍♂️ Checking if retweet is reply')
+            if (!isReply(data.statuses[rando])) {
+                logger.trace('🐦 Retweeting')
+
+                logger.debug('Tweet is not a reply, setting retweetId')
+                retweetId = data.statuses[rando].id_str
+                logger.debug(`retweetId: ${retweetId}`)
+
+                logger.trace('Posting Tweet')
+                bot.post(
+                    'statuses/retweet/:id', {
+                        id: retweetId
+                    },
+                    (err, response) => {
+                        if (err) {
+                            logger.error(
+                                'An error occurred posting the tweet with the tweet: ',
                                 data.statuses[rando].text,
-                                'RANDO ID: ',
-                                rando
+                                'ERROR MSG: ',
+                                err
                             )
                         }
-                    )
-                }
+                        logger.debug(
+                            'SUCCESS: RT: ',
+                            data.statuses[rando].text,
+                            'RANDO ID: ',
+                            rando
+                        )
+                    }
+                )
             }
         }
     )
