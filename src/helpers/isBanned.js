13a0ea9cd2d098a259cf9a07dd70f3c4b4f5f502
Ryan Valizan <ryan@vnetkc.com>
2020-10-18 23:17:13 -0500

    :sparkles: added logs and improved match filter for illegal terms

--- src/helpers/isBanned.js
+++ src/helpers/isBanned.js
@@ -1,7 +1,48 @@
+const log4jConfig = require('../log4jConfig')
+
+// Start Logging
+const config = require('../config')
+const log4js = require('log4js')
+log4js.configure(log4jConfig.options)
+const logger = log4js.getLogger('default.tweets')
+
+// Set log levels to match user env file setting
+logger.level = config.envVars.logLevel.toUpperCase()
+logger.trace('📝 Starting to log: inBanned.js')
+
+let word
 const isBanned = tweet => {
-    const BannedWords = require('./strings').blackListedWords
-    if (BannedWords.some(tweet.text.match.bind(tweet.text)))
-        return true
+    logger.trace('🟢 Starting No Banned Words Validation')
+    const BannedWords = require('./strings').blackListedRetweetWords
+    logger.trace(`🔃 Reset to BannedWords Var to false`)
+    let bannedWords = false
+    logger.debug(`The bannedWords var is now: ${bannedWords}`)
+
+    logger.trace('🕵️‍♂️ Checking Tweet')
+    logger.debug('📝 Tweet being evaluated: ' + tweet.text)
+    if (BannedWords.some(badWord => storeWordAndBind(tweet.text, badWord))) {
+        logger.debug(`🚫 Banned Word Found: ${word}`)
+        bannedWords = true
+        logger.debug(`The bannedWords var is now: ${bannedWords}`)
+        logger.trace('✌️ Dipping out to maybe find another tweet.')
+    }
+    return bannedWords
+}
+
+function storeWordAndBind(text, badWord) {
+    config.envVars.traceStoreAndBind ? logger.trace('📦 Binding and storing strings') : ''
+    let ret = false
+    config.envVars.traceStoreAndBind ? logger.trace(`Word is currently: ${word}`) : ''
+    config.envVars.traceStoreAndBind ? logger.trace(`Var badWord being evaluated is: ${badWord}`) : ''
+    word = badWord
+    config.envVars.traceStoreAndBind ? logger.trace(`Word is now: ${word}`) : ''
+    config.envVars.traceStoreAndBind ? logger.debug(`Is this statement '${word}' found in: ${text}`) : ''
+
+    if (text.match(word))
+        ret = true
+
+    config.envVars.traceStoreAndBind ? logger.debug(`ret in StoreWordAndBind is being sent back as: ${ret} where false is good news`) : ''
+    return ret
 }
 
 module.exports = isBanned
