1d8a251aacb1ce6c87850e4c662201e9a9e63f88
Ryan Valizan <ryan@vnetkc.com>
2020-10-18 23:18:46 -0500

    :loud_sound: added logging and some extra validations

--- src/api/retweet.js
+++ src/api/retweet.js
@@ -9,7 +9,9 @@ const log4jConfig = require('../log4jConfig')
 const consoleLol = require('console.lol')
 const log4js = require('log4js')
 log4js.configure(log4jConfig.options)
-const logger = log4js.getLogger('default.tweets')
+let logger = log4js.getLogger('default')
+if (config.envVars.tweetLog)
+    logger = log4js.getLogger('default.tweets')
 
 // Set log levels to match user env file setting
 logger.level = config.envVars.logLevel.toUpperCase()
@@ -21,9 +23,15 @@ const queryString = unique(param.queryString.split(','))
 const bot = new Twit(config.twitterKeys)
 
 const retweet = () => {
-    logger.trace('🟢 Retweet Starting')
+    logger.trace('✨ START NEW RETWEET ✨')
     const query = queryString()
 
+    logger.trace('📝 searching tweets')
+    logger.info(`Performing query search for: ${query}`)
+    logger.debug(
+        `result_type: ` + param.resultType + ' |---| ',
+        `count: ` + param.searchCount
+    )
     bot.get(
         'search/tweets', {
             q: query,
@@ -34,21 +42,37 @@ const retweet = () => {
         },
         (err, data, response) => {
             if (err) {
-                logger.error('Search for tweets failed with message: ', err)
+                logger.error('Search for tweets failed with error message: ', err)
+                logger.info('🗑 Search Tossed, Resetting Query')
                 return
             }
+            if (data.statuses.length === 0) {
+                logger.warn('💥 data length is 0')
+                logger.info('🗑 Search Tossed, Resetting Query')
+                return
+            }
+            logger.debug(`Query completed successfully with data: ${data}`)
             let retweetId
 
             // grab random tweet ID number to retweet - desired range for random number is [0..data.statuses.length-1]
             logger.trace('🟢 Starting search for random post to check')
             let rando = Math.floor(Math.random() * data.statuses.length)
             logger.debug(`Rando was assigned to: ${rando}`)
-            while (isBanned(data.statuses[rando])) {
-                logger.trace('Getting new rando number')
+            let skip = 0
+            while (isBanned(data.statuses[rando]) || data.statuses[rando].text.length < 75) {
+                if (skip > config.envVars.skipCount) {
+                    logger.debug('🚮 SKIP TOO HIGH')
+                    logger.debug(`📝 Skip is ${skip} now which is greater than the env var of ` + config.envVars.skipCount)
+                    logger.info('🗑 Search Tossed, Resetting Query')
+                    return
+                }
+                logger.trace('🔃 Getting new rando')
                 rando = Math.floor(Math.random() * data.statuses.length)
                 logger.debug(`Rando is now set to ${rando}`)
+                skip++
+                logger.debug(`Skip is now at: ${skip}`)
             }
-            logger.trace('While loop checking tweet for illegal terms has completed')
+            logger.trace('🏁 While loop checking tweet for illegal terms has completed')
             logger.debug(`While loop completed successfully with rando: ${rando}`)
 
             logger.trace('🕵️‍♂️ Checking if retweet is reply')
@@ -59,29 +83,44 @@ const retweet = () => {
                 retweetId = data.statuses[rando].id_str
                 logger.debug(`retweetId: ${retweetId}`)
 
-                logger.trace('Posting Tweet')
-                bot.post(
-                    'statuses/retweet/:id', {
-                        id: retweetId
-                    },
-                    (err, response) => {
-                        if (err) {
-                            logger.error(
-                                'An error occurred posting the tweet with the tweet: ',
+                logger.trace('🐦 Posting Tweet')
+
+
+
+                /**
+                 * ! Be sure to remove this!
+                 */
+                logger.info('📰 RETWEETED: ' + data.statuses[rando].text)
+                    /**
+                     * ! Be sure to remove this!
+                     
+                    bot.post(
+                        'statuses/retweet/:id', {
+                            id: retweetId
+                        },
+                        (err, response) => {
+                            logger.info('📰 RETWEETED: ' + data.statuses[rando].text)
+                            if (err) {
+                                logger.error(
+                                    'An error occurred posting the tweet with the tweet: ',
+                                    data.statuses[rando].text,
+                                    'ERROR MSG: ',
+                                    err
+                                )
+                                return
+                            }
+                            logger.debug(
+                                'SUCCESS: RT: ',
                                 data.statuses[rando].text,
-                                'ERROR MSG: ',
-                                err
+                                'RANDO ID: ',
+                                rando
                             )
                         }
-                        logger.debug(
-                            'SUCCESS: RT: ',
-                            data.statuses[rando].text,
-                            'RANDO ID: ',
-                            rando
-                        )
-                    }
-                )
+                    )
+                    */
+                return
             }
+            logger.info('🗑 Search Tossed, Resetting Query')
         }
     )
 }
