7b27b4882afb834b31a5725c16eeb221bc7b72d7
Ryan Valizan <ryan@vnetkc.com>
2020-10-20 19:41:45 -0500

    🏗 Updates to rando module which we may re-impliment soon

--- src/rando.js
+++ src/rando.js
@@ -1,6 +1,18 @@
-const rando = (arr) => {
+/**
+ * @module helpers/rando
+ */
+function rando(arr) {
     const index = Math.floor(Math.random() * arr.length)
     return arr[index]
 }
 
-module.exports = rando
+export {
+    /**
+     * @module helpers/rando
+     * This generates a random number for selecting a tweet in the array.
+     * @function rando
+     * @param {number} arr - Max number the rando number returned can be.
+     * @returns {number} A number between 0 and arr. 
+     */
+    rando
+}
