839bd603656bbf510d7528995a4ba6339b8dea12
Ryan Valizan <ryan@vnetkc.com>
2020-10-21 20:56:38 -0500

    :bulb: adding JSDoc comments to files

--- src/helpers/isBanned.js
+++ src/helpers/isBanned.js
@@ -1,7 +1,17 @@
-const log4jConfig = require('../log4jConfig')
+/**
+ * @module helpers/isBanned
+ * @desc Validates the tweet message looking for illegal words and terms, tossing out any matches it finds as bad tweets.
+ * @param {object} tweet - Twitter message being brought in for evaluation.
+ * @returns {Boolean} If any of the reply checks come back as true then return false otherwise return true that it is a reply. 
+ * @requires module:config
+ * @requires module:log4js
+ * @requires module:log4jConfig
+ */
+
+const config = require('../config')
 
 // Start Logging
-const config = require('../config')
+const log4jConfig = require('../log4jConfig')
 const log4js = require('log4js')
 log4js.configure(log4jConfig.options)
 const logger = log4js.getLogger('default.tweets')
@@ -10,6 +20,7 @@ const logger = log4js.getLogger('default.tweets')
 logger.level = config.envVars.logLevel.toUpperCase()
 logger.trace('📝 Starting to log: inBanned.js')
 
+//** word is always the current word being tested, and if found is added to the logs when enabled.*/
 let word
 const isBanned = tweet => {
     logger.trace('🟢 Starting No Banned Words Validation')
@@ -29,6 +40,13 @@ const isBanned = tweet => {
     return bannedWords
 }
 
+/**
+ * @function storeWordAndBind
+ * @desc Takes a tweet message and an illegal or banned retweet term and then looks to see if there is a match.
+ * @param {string} text The tweet message to be evaluated.
+ * @param {string} badWord The RegEx string to look for in the tweet.
+ * @returns {Boolean} If the word is bad it returns true, otherwise returns false.
+ */
 function storeWordAndBind(text, badWord) {
     config.envVars.traceStoreAndBind ? logger.trace('📦 Binding and storing strings') : ''
     let ret = false
--- src/log4jConfig.js
+++ src/log4jConfig.js
@@ -1,12 +1,13 @@
-/**
- * log4jConfig module.
- * @desc This module exports the log4j configuration options for the project
- * @module helpers/log4jConfig
- * @requires module:helpers/config
- */
 const config = require('./config')
-
 module.exports = {
+    /**
+     * @module log4jConfig
+     * @desc This module exports the log4j configuration options for the project
+     * @requires module:config
+     * @param {object} options These are various log options available 
+     * @param {object} options.categories.default Wires up logging files for bot, errors and the console. Create with logger.getLogger()
+     * @param {object} options.categories.default.tweets Adds tweet log to everything else along with default category. Create with logger.getLogger('default.tweets')
+     */
     options: {
         appenders: {
             tweetsLog: {
